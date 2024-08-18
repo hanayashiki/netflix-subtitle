650525880e205874e3e28d44f3d8d64a8521bf6f
hanayashiki <wangchenyu2017@gmail.com>
2024-08-18 17:06:03 +0900

    feat: add copy

--- scripts/content.js
+++ scripts/content.js
@@ -1,19 +1,39 @@
 console.log("Neflix-Subtitle: Content script loaded");
 
+const getSubtitle = () => {
+  const subtitle = document.querySelector('.player-timedtext')?.innerText.trim();
+
+  if (!subtitle) {
+    throw new Error("Neflix-Subtitle: No subtitle found");
+  }
+
+  return subtitle;
+}
+
+document.addEventListener("copy", async function (e) {
+  e.clipboardData.setData("text/plain", getSubtitle());
+  event.preventDefault();
+});
+
 const observer = new MutationObserver((mutationsList, observer) => {
   const player = document.querySelector('*[data-uia="player"]');
 
   if (player) {
     console.log("Neflix-Subtitle: Player found");
-    player.addEventListener("keypress", function (e) {
-      if (e.ctrlKey && e.code === "KeyB" && !e.isComposing) {
+    player.addEventListener("keypress", async function (e) {
+      if (e.ctrlKey && e.code === "KeyZ" && !e.isComposing) {
         const subtitle = document.querySelector('.player-timedtext').innerText.trim();
 
         if (subtitle.length === 0) {
           alert("Neflix-Subtitle: No subtitle found");
+          return;
         }
 
-        const keyword = "Explain: " + subtitle;
+        await navigator.clipboard.writeText(subtitle);
+      }
+
+      if (e.ctrlKey && e.code === "KeyB" && !e.isComposing) {
+        const keyword = "Explain: " + getSubtitle();
         window.open(`https://chatgpt.com?q=${encodeURIComponent(keyword)}`);
       }
     });
